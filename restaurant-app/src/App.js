22a28085e7b97c93a6ed17e5679d5f5717bf4a1a
cutebid <67562053+cutebid@users.noreply.github.com>
2022-04-26 20:57:56 +0100

    food restaurant

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import Products from './pages/Products';
 
 const App = () => {
   return (
-    <div>
+    <div className="appp">
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/Products" element={<Products />} />
         </Routes>
       </Router>
     </div>
---
+++ restaurant-app/src/productCategory.js
@@ -0,0 +1,16 @@
+const lists = [
+  {
+    id: 1,
+    name: "Food",
+  },
+  {
+    id: 2,
+    name: "Desserts",
+  },
+  {
+    id: 3,
+    name: "Drinks",
+  },
+];
+
+export default lists;
